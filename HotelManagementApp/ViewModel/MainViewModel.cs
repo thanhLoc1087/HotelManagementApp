05ec42433063ddefa443d4fbf366ebf2d444a0f4
Trung Hieu Nguyen Duong <mobile.hieunguyenduong@gmail.com>
2022-11-03 15:57:06 +0700

    setup mvvm and finnished login screen

---
+++ HotelManagementApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using HotelManagementApp.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HotelManagementApp.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public bool IsLoaded = false;
+        public MainViewModel()
+        {
+            if (IsLoaded)
+            {
+                IsLoaded = true;
+                LoginView loginView = new LoginView();
+                loginView.ShowDialog();
+            }
+        }
+    }
+}
