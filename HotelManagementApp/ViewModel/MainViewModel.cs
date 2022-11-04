ac8693650b8eea0afc760624328ac438e24757fa
hieundt <19521502@gm.uit.edu.vn>
2022-11-04 18:58:16 +0700

    finnished login view and added UC bar

--- HotelManagementApp/ViewModel/MainViewModel.cs
+++ HotelManagementApp/ViewModel/MainViewModel.cs
@@ -5,20 +5,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace HotelManagementApp.ViewModel
 {
+    
     public class MainViewModel : BaseViewModel
     {
+        public ICommand LoadedWindowCommand { get; set; }   
         public bool IsLoaded = false;
         public MainViewModel()
         {
-            if (IsLoaded)
+            LoadedWindowCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
             {
                 IsLoaded = true;
                 LoginView loginView = new LoginView();
                 loginView.ShowDialog();
             }
+            );
         }
     }
 }
