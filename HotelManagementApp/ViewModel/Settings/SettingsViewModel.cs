2d1b009bfaf977040130be57d103b37ea9c5f308
Khoadeptrai <96755096+dkdeptrai@users.noreply.github.com>
2022-12-05 11:39:01 +0700

    new files

--- HotelManagementApp/ViewModel/Settings/SettingsViewModel.cs
+++ HotelManagementApp/ViewModel/Settings/SettingsViewModel.cs
@@ -1,7 +1,9 @@
-﻿using HotelManagementApp.View;
+﻿using HotelManagementApp.Model;
+using HotelManagementApp.View;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,7 +13,14 @@ namespace HotelManagementApp.ViewModel
 {
     public class SettingsViewModel : BaseViewModel
     {
+        private string _CurrentPassword;
+        public string CurrentPassword { get => _CurrentPassword; set { _CurrentPassword = value; OnPropertyChanged(); } }
+        private string _NewPassword;
+        public string NewPassword { get => _NewPassword; set { _NewPassword = value; OnPropertyChanged(); } }
+        private string _ConfirmPassword;
+        public string ConfirmPassword { get => _ConfirmPassword; set { _ConfirmPassword = value; OnPropertyChanged(); } }
         public ICommand LogoutCommand { get; set; }
+        public ICommand ChangePasswordCommand { get; set; }
 
         public SettingsViewModel()
         {
@@ -21,6 +30,48 @@ namespace HotelManagementApp.ViewModel
                 Application.Current.Shutdown();
             }
             );
+
+            ChangePasswordCommand = new RelayCommand<object>((p) => 
+            {
+                var hashedPassword = MD5Hash(Base64Encode(CurrentPassword));
+                if(ConfirmPassword != NewPassword)
+                {
+                    return false;
+                }
+                //if(hashedPassword != CurrentEmployee.PasswordHash)
+                //{
+                //    return false;
+                //}
+                
+                return true;
+            }, (p) =>
+            {
+                //var staff = DataProvider.Instance.DB.Staffs.Where(x => x.ID == CurrentEmployee.ID).FirstOrDefault();
+                var hashedPassword = MD5Hash(Base64Encode(NewPassword));
+                // CurrentEmployee.PasswordHash = hashedPassword;
+                DataProvider.Instance.DB.SaveChanges();
+            }
+            );
+        }
+
+        private string MD5Hash(string input)
+        {
+            StringBuilder hash = new StringBuilder();
+            MD5CryptoServiceProvider md5provider = new MD5CryptoServiceProvider();
+            byte[] bytes = md5provider.ComputeHash(new UTF8Encoding().GetBytes(input));
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                hash.Append(bytes[i].ToString("x2"));
+            }
+            return hash.ToString();
+        }
+
+        private string Base64Encode(string input)
+        {
+            var textBytes = Encoding.UTF8.GetBytes(input);
+            var base64String = Convert.ToBase64String(textBytes);
+            return base64String;
         }
     }
 }
