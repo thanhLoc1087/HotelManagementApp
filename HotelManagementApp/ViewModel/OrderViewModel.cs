4b63beaedfa34c3d0affdf985ee6c5449fab419d
Khoadeptrai <96755096+dkdeptrai@users.noreply.github.com>
2022-12-28 20:33:30 +0700

    Update OrderViewModel.cs

--- HotelManagementApp/ViewModel/OrderViewModel.cs
+++ HotelManagementApp/ViewModel/OrderViewModel.cs
@@ -162,8 +162,6 @@ namespace HotelManagementApp.ViewModel
                     DBbill.Orders.Add(item);
                     DBbill.TotalMoney += item.TotalPrice;
                     Global.OrdersList.Add(item);
-                    Sbill.Orders.Add(item);
-                    Sbill.TotalMoney += item.TotalPrice;
                 }
                 DataProvider.Instance.DB.SaveChanges();
                 RoomNum = null;
